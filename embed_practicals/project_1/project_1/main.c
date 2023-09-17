7a8da00389c06e1c458136c5f4fa0f0dec5d6440
Bryan <baliddeki@gmail.com>
2023-09-17 09:23:08 +0300

    added folder for practicals

---
+++ embed_practicals/project_1/project_1/main.c
@@ -0,0 +1,18 @@
+/*
+ * project_1.c
+ *
+ * Created: 9/14/2023 9:25:16 PM
+ * Author : ari
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
