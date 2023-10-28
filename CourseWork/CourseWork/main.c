a26aea5f4bb0a9e0cc6b2f1534a42721e394ba96
baliddeki <baliddeki@gmail.com>
2023-10-28 10:13:47 +0300

    code for embedded systems coursework

---
+++ CourseWork/CourseWork/main.c
@@ -0,0 +1,25 @@
+/*
+ * CourseWork.c
+ *
+ * Created: 10/28/2023 8:41:17 AM
+ * Author : ari
+ */ 
+
+#include <avr/io.h>
+
+// Tourist Attendance
+
+void buzz(){
+	#define PK1 as TAS // TAS - tourist arrival switch
+	
+}
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
