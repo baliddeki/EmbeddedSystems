692a0314a123585c929068d93d714757ee27cb8d
baliddeki <baliddeki@gmail.com>
2023-10-28 10:59:44 +0300

    coursework folder'

--- CourseWork/CourseWork/main.c
+++ CourseWork/CourseWork/main.c
@@ -7,10 +7,23 @@
 
 #include <avr/io.h>
 
-// Tourist Attendance
+/*
+Method for buzzer to sound when tourist arrives
+*/
 
 void buzz(){
-	#define PK1 as TAS // TAS - tourist arrival switch
+	#define TAS PK1  // TAS - tourist arrival switch
+	#define TAB PJ3  // TAB - tourist arrival buzz
+	
+	 DDRK &= ~(1 << TAS);
+	 
+	 DDRJ |= (1 << TAB);
+	 
+	 if ((PINK & (1 << TAS)) == 0) {
+		 // Toggle the state of the buzzer pin
+		 PORTJ ^= (1 << TAB);
+	 }
+	
 	
 }
 
@@ -20,6 +33,7 @@ int main(void)
     /* Replace with your application code */
     while (1) 
     {
+		buzz();
     }
 }
 
